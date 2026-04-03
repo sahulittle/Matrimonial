7ec1705b659845ceea0327c1e524d36582a398f5
Gyana Singh <gyan123priya@gmail.com>
2026-04-04 00:16:53 +0530

    hii

--- src/components/common/Preloader.jsx
+++ src/components/common/Preloader.jsx
@@ -6,7 +6,12 @@ const Preloader = () => {
     <div className="preloader">
       <div className="heart-wrapper">
         <div className="heart">
-          <img src="/favicon.png" alt="Loading..." className="logo" />
+          <img
+            src="/favicon.png"
+            alt="Loading..."
+            className="preloader-logo"
+            aria-hidden="false"
+          />
         </div>
       </div>
     </div>
