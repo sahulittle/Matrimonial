968c41e18e6044a0edba17625c98a49e76a9bc14
Gyana Priyadarsan Singh <gyan123priya@gmail.com>
2026-05-09 12:33:24 +0530

    hi

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#fdf2f8",
+          100: "#fce7f3",
+          200: "#fbcfe8",
+          300: "#f9a8d4",
+          400: "#f472b6",
+          500: "#ec4899", // pink-500
+          600: "#db2777", // pink-600
+          700: "#be185d",
+          800: "#9d174d",
+          900: "#831843",
+          950: "#500724",
+        },
+      },
+    },
   },
   plugins: [],
 }
